041b4e35458ec2418e8cf11a75b54c366e0b70e4
sharpsw <sharpsw@gmail.com>
2013-03-30 17:52:16 -0700

    Added database and began writing fetch request for Users.

--- TheRuckus/TheRuckus/FirstViewController.m
+++ TheRuckus/TheRuckus/FirstViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "FirstViewController.h"
-
+#import "User.h"
 @interface FirstViewController ()
 
 @end
@@ -18,16 +18,60 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+   
     
-    // Read in the dataModel
+    // Read in RuckusDataMode.xcdatamodeld
     model = [NSManagedObjectModel mergedModelFromBundles:nil];
-    NSLog(@"Model: %@", model);
+    NSLog(@"model = %@", model);
     
-    //Opens a SQLite database at a particular filename
-    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc]initWithManagedObjectModel:model];
+    // Used to open a SQLite database at a particular file name
+    NSPersistentStoreCoordinator *psc =
+    [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
     
+    // Where does the SQLite file go?
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* basePath = [paths objectAtIndex:0];
+    NSURL* storeUrl = [NSURL fileURLWithPath:[basePath stringByAppendingPathComponent: @"RuckusDatabase.sqlite"]];
+    NSLog(@"storeUrl = %@", [storeUrl description]);
+
+    NSError *error = nil;
     
+    // Add a new SQLite database that the PersistentStoreCoordinator interacts with
+    if (![psc addPersistentStoreWithType:NSSQLiteStoreType
+                           configuration:nil
+                                     URL:storeUrl
+                                 options:nil
+                                   error:&error]) {
+        [NSException raise:@"Open failed"
+                    format:@"Reason: %@", [error localizedDescription]];
+    }
     
+    // Create the managed object context
+    context = [[NSManagedObjectContext alloc] init];
+    [context setPersistentStoreCoordinator:psc];
+    
+    // The managed object context can manage undo, but we don't need it
+    [context setUndoManager:nil];
+}
+
+-(void) fetchUsers
+{
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    NSEntityDescription *e = [[model entitiesByName] objectForKey:@"Name"];
+    [request setEntity:e];
+    
+    NSError *error;
+    NSArray *result = [context  executeFetchRequest:request error:&error];
+}
+
+- (BOOL)saveChanges
+{
+    NSError *err = nil;
+    BOOL successful = [context save:&err];
+    if (!successful) {
+        NSLog(@"Error saving: %@", [err localizedDescription]);
+    }
+    return successful;
 }
 
 - (void)didReceiveMemoryWarning
